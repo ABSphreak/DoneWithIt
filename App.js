2fa44b83a0a1ef3616912123c79bc368357ec959
ABSphreak <absphreak@outlook.com>
2021-06-07 01:55:49 +0530

    ➕ Add more code

--- App.js
+++ App.js
@@ -1,21 +1,7 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { View } from 'react-native';
+import WelcomeScreen from './app/screens/WelcomeScreen';
 
 export default function App() {
-	return (
-		<View style={styles.container}>
-			<Text>Aviral SUCKS</Text>
-			<StatusBar style="auto" />
-		</View>
-	);
+	return <WelcomeScreen />;
 }
-
-const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: '#c77dff',
-		alignItems: 'center',
-		justifyContent: 'center',
-	},
-});
