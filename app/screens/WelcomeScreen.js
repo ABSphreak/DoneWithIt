8458471beb10295a277f95871f75bc992ad2c5ea
ABSphreak <absphreak@outlook.com>
2021-06-29 20:45:06 +0530

    🔧 Swap fake buttons with real ones

--- app/screens/WelcomeScreen.js
+++ app/screens/WelcomeScreen.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Image, ImageBackground, StyleSheet, Text, View } from 'react-native';
+import AppButton from '../components/AppButton';
 import colors from '../config/colors';
 
 function WelcomeScreen(props) {
@@ -9,23 +10,19 @@ function WelcomeScreen(props) {
 				<Image style={styles.logo} source={require('../assets/logo.png')} />
 				<Text style={styles.logoText}>Sell what you don't need.</Text>
 			</View>
-			<View style={styles.loginButton}></View>
-			<View style={styles.registerButton}></View>
+			<View style={styles.buttonsContainer}>
+				<AppButton title="Login" />
+				<AppButton title="Register" color="secondary" />
+			</View>
 		</ImageBackground>
 	);
 }
 
 const styles = StyleSheet.create({
 	background: { flex: 1, justifyContent: 'flex-end', alignItems: 'center' },
-	loginButton: {
+	buttonsContainer: {
+		padding: 20,
 		width: '100%',
-		height: 79,
-		backgroundColor: colors.primary,
-	},
-	registerButton: {
-		width: '100%',
-		height: 79,
-		backgroundColor: colors.secondary,
 	},
 	logo: {
 		width: 100,
