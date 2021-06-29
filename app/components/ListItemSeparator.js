1069888c748806f4c0e86043b20d264dfb73d5ee
ABSphreak <absphreak@outlook.com>
2021-06-30 00:14:31 +0530

    ➕ ListItemSeparator component

---
+++ app/components/ListItemSeparator.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+import colors from '../config/colors';
+
+function ListItemSeparator() {
+	return <View style={styles.separator} />;
+}
+
+const styles = StyleSheet.create({
+	separator: { width: '100%', height: 1, backgroundColor: colors.lightGray },
+});
+
+export default ListItemSeparator;
