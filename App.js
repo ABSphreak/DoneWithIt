e5ebe0a4b0d35b43551e7e03327db60d5d1e821f
ABSphreak <absphreak@outlook.com>
2021-06-29 19:18:47 +0530

    ➕ Added second view

--- App.js
+++ App.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import { View } from 'react-native';
 import WelcomeScreen from './app/screens/WelcomeScreen';
+import ViewImageScreen from './app/screens/ViewImageScreen';
 
 export default function App() {
-	return <WelcomeScreen />;
+	return (
+		<>
+			<WelcomeScreen />
+			<ViewImageScreen />
+		</>
+	);
 }
