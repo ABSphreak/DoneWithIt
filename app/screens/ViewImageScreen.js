a3969590d1cbed67bcb4c510711d4c492509ce30
ABSphreak <absphreak@outlook.com>
2021-06-29 23:19:16 +0530

    ➕ Added real material icons

--- app/screens/ViewImageScreen.js
+++ app/screens/ViewImageScreen.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import { Image, StyleSheet, View } from 'react-native';
 import colors from '../config/colors';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 function ViewImageScreen(props) {
 	return (
 		<View style={styles.container}>
-			<View style={styles.closeIcon}></View>
-			<View style={styles.deleteIcon}></View>
+			<View style={styles.closeIcon}>
+				<MaterialCommunityIcons name="close" color="white" size={35} />
+			</View>
+			<View style={styles.deleteIcon}>
+				<MaterialCommunityIcons name="trash-can-outline" color="white" size={35} />
+			</View>
 			<Image resizeMode="contain" style={styles.image} source={require('../assets/chair.jpg')} />
 		</View>
 	);
@@ -14,9 +19,6 @@ function ViewImageScreen(props) {
 
 const styles = StyleSheet.create({
 	closeIcon: {
-		width: 50,
-		height: 50,
-		backgroundColor: colors.primary,
 		position: 'absolute',
 		top: 40,
 		left: 30,
@@ -25,9 +27,6 @@ const styles = StyleSheet.create({
 		backgroundColor: colors.black,
 	},
 	deleteIcon: {
-		width: 50,
-		height: 50,
-		backgroundColor: colors.secondary,
 		position: 'absolute',
 		top: 40,
 		right: 30,
