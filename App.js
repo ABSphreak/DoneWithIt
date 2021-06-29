76c60a2c6e2af0abb2e561e85aa28961db0f2640
ABSphreak <absphreak@outlook.com>
2021-06-29 20:44:21 +0530

    ➕ Added code for testing

--- App.js
+++ App.js
@@ -1,13 +1,25 @@
 import React from 'react';
-import { View } from 'react-native';
+// import { View } from 'react-native';
+
 import WelcomeScreen from './app/screens/WelcomeScreen';
-import ViewImageScreen from './app/screens/ViewImageScreen';
+// import ViewImageScreen from './app/screens/ViewImageScreen';
+// import TestViewScreen from './app/screens/TestViewScreen';
+// import AppButton from './app/components/AppButton';
 
 export default function App() {
 	return (
 		<>
 			<WelcomeScreen />
-			<ViewImageScreen />
+			{/* <ViewImageScreen /> */}
+			{/* <TestViewScreen /> */}
+			{/* <View
+				style={{
+					flex: 1,
+					justifyContent: 'center',
+					alignItems: 'center',
+				}}>
+				<AppButton title="Login" onPress={() => console.log('Button pressed')} />
+			</View> */}
 		</>
 	);
 }
