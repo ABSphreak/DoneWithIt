8de8482f039bffa290230dfe1f0af019a09f452b
ABSphreak <absphreak@outlook.com>
2021-06-30 00:13:48 +0530

    ➕ Add Reusable Screen component

---
+++ app/components/Screen.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Constants from 'expo-constants';
+import { SafeAreaView, StyleSheet } from 'react-native';
+
+function Screen({ children }) {
+	return <SafeAreaView style={styles.screen}>{children}</SafeAreaView>;
+}
+
+const styles = StyleSheet.create({
+	screen: {
+		paddingTop: Constants.statusBarHeight,
+		flex: 1,
+	},
+});
+
+export default Screen;
