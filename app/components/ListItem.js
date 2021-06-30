76421eb0200bef922c6aad9d321bdb53620868f2
ABSphreak <absphreak@outlook.com>
2021-06-30 20:03:57 +0530

    🔧 Refactor ListItem component

--- app/components/ListItem.js
+++ app/components/ListItem.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Image, StyleSheet, View, TouchableHighlight } from 'react-native';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 import { Swipeable } from 'react-native-gesture-handler';
 
 import AppText from './AppText';
@@ -13,9 +14,16 @@ function ListItem({ title, subTitle, image, IconComponent, onPress, renderRightA
 					{IconComponent}
 					{image && <Image style={styles.image} source={image} />}
 					<View style={styles.detailsContainer}>
-						<AppText style={styles.title}>{title}</AppText>
-						{subTitle && <AppText style={styles.subTitle}>{subTitle}</AppText>}
+						<AppText style={styles.title} numberOfLines={1}>
+							{title}
+						</AppText>
+						{subTitle && (
+							<AppText style={styles.subTitle} numberOfLines={2}>
+								{subTitle}
+							</AppText>
+						)}
 					</View>
+					<MaterialCommunityIcons name="chevron-right" size={25} color={defaultStyles.colors.medium} />
 				</View>
 			</TouchableHighlight>
 		</Swipeable>
@@ -25,10 +33,12 @@ function ListItem({ title, subTitle, image, IconComponent, onPress, renderRightA
 const styles = StyleSheet.create({
 	container: {
 		flexDirection: 'row',
+		alignItems: 'center',
 		padding: 15,
 		backgroundColor: defaultStyles.colors.white,
 	},
 	detailsContainer: {
+		flex: 1,
 		marginLeft: 10,
 		justifyContent: 'center',
 	},
