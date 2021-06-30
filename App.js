0e963873ebf2f3a80c5686bbcafe688ead26efb1
ABSphreak <absphreak@outlook.com>
2021-06-30 20:06:10 +0530

    🔧 Fix App JS (incomplete)

--- App.js
+++ App.js
@@ -1,25 +1,6 @@
 import React from 'react';
-// import { View } from 'react-native';
-
-import WelcomeScreen from './app/screens/WelcomeScreen';
-// import ViewImageScreen from './app/screens/ViewImageScreen';
-// import TestViewScreen from './app/screens/TestViewScreen';
-// import AppButton from './app/components/AppButton';
+import ListingEditScreen from './app/screens/ListingEditScreen';
 
 export default function App() {
-	return (
-		<>
-			<WelcomeScreen />
-			{/* <ViewImageScreen /> */}
-			{/* <TestViewScreen /> */}
-			{/* <View
-				style={{
-					flex: 1,
-					justifyContent: 'center',
-					alignItems: 'center',
-				}}>
-				<AppButton title="Login" onPress={() => console.log('Button pressed')} />
-			</View> */}
-		</>
-	);
+	return <ListingEditScreen />;
 }
