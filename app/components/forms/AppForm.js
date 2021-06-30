f24c4177d6b7805c045e062c14e1dee4e04d00e9
ABSphreak <absphreak@outlook.com>
2021-06-30 20:03:07 +0530

    ➕ Add Custom Form component

---
+++ app/components/forms/AppForm.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Formik } from 'formik';
+
+function AppForm({ initialValues, onSubmit, validationSchema, children }) {
+	return (
+		<Formik initialValues={initialValues} onSubmit={onSubmit} validationSchema={validationSchema}>
+			{() => <>{children}</>}
+		</Formik>
+	);
+}
+
+export default AppForm;
