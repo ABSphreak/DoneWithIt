33f839a7b935f7a46d2eef1d0942daaa3e1745b1
ABSphreak <absphreak@outlook.com>
2021-06-30 14:16:19 +0530

    ➕ Add reusable styles on screen

--- app/components/Screen.js
+++ app/components/Screen.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import Constants from 'expo-constants';
-import { SafeAreaView, StyleSheet } from 'react-native';
+import { SafeAreaView, StyleSheet, View } from 'react-native';
 
-function Screen({ children }) {
-	return <SafeAreaView style={styles.screen}>{children}</SafeAreaView>;
+function Screen({ children, style }) {
+	return (
+		<SafeAreaView style={[styles.screen, style]}>
+			<View style={style}>{children}</View>
+		</SafeAreaView>
+	);
 }
 
 const styles = StyleSheet.create({
