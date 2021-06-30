3457efa3253ebaa90144c2edd422a6dbfd6e83c4
ABSphreak <absphreak@outlook.com>
2021-06-30 14:38:22 +0530

    ➕ Add Custom Text Input component

---
+++ app/components/AppTextInput.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { StyleSheet, TextInput, View } from 'react-native';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import defaultStyles from '../config/styles';
+
+function AppTextInput({ icon, ...otherProps }) {
+	return (
+		<View style={styles.container}>
+			{icon && <MaterialCommunityIcons name={icon} size={20} color={colors.medium} style={styles.icon} />}
+			<TextInput style={defaultStyles.text} {...otherProps} />
+		</View>
+	);
+}
+
+const styles = StyleSheet.create({
+	continaer: {
+		backgroundColor: defaultStyles.colors.lightGray,
+		borderRadius: 25,
+		flexDirection: 'row',
+		width: '100%',
+		padding: 15,
+		marginVertical: 10,
+	},
+	icon: {
+		marginRight: 10,
+	},
+});
+
+export default AppTextInput;
