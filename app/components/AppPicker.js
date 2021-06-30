49304b89bf760d042e82721f9c14d00157945d7e
ABSphreak <absphreak@outlook.com>
2021-06-30 20:03:31 +0530

    ➕ Add Picker component

---
+++ app/components/AppPicker.js
@@ -0,0 +1,65 @@
+import React, { useState } from 'react';
+import { Modal, StyleSheet, TouchableWithoutFeedback, View, Button, FlatList } from 'react-native';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import defaultStyles from '../config/styles';
+import AppText from './AppText';
+import Screen from './Screen';
+import PickerItem from './PickerItem';
+
+function AppPicker({ icon, items, numberOfColumns = 1, onSelectItem, PickerItemComponent = PickerItem, placeholder, selectedItem, width = '100%' }) {
+	const [modalVisible, setModalVisible] = useState(false);
+
+	return (
+		<>
+			<TouchableWithoutFeedback onPress={() => setModalVisible(true)}>
+				<View style={[styles.container, { width }]}>
+					{icon && <MaterialCommunityIcons name={icon} size={20} color={defaultStyles.colors.medium} style={styles.icon} />}
+					{selectedItem ? <AppText style={styles.text}>{selectedItem.label}</AppText> : <AppText style={styles.placeholder}>{placeholder}</AppText>}
+					<MaterialCommunityIcons name="chevron-down" size={20} color={defaultStyles.colors.medium} />
+				</View>
+			</TouchableWithoutFeedback>
+			<Modal visible={modalVisible} animationType="slide">
+				<Screen>
+					<Button title="Close" onPress={() => setModalVisible(false)} />
+					<FlatList
+						data={items}
+						numColumns={numberOfColumns}
+						keyExtractor={item => item.value.toString()}
+						renderItem={({ item }) => (
+							<PickerItemComponent
+								item={item}
+								label={item.label}
+								onPress={() => {
+									setModalVisible(false);
+									onSelectItem(item);
+								}}
+							/>
+						)}
+					/>
+				</Screen>
+			</Modal>
+		</>
+	);
+}
+
+const styles = StyleSheet.create({
+	container: {
+		backgroundColor: defaultStyles.colors.lightGray,
+		borderRadius: 25,
+		flexDirection: 'row',
+		padding: 15,
+		marginVertical: 10,
+	},
+	icon: {
+		marginRight: 10,
+	},
+	placeholder: {
+		flex: 1,
+		color: defaultStyles.colors.medium,
+	},
+	text: {
+		flex: 1,
+	},
+});
+
+export default AppPicker;
